1892156cc7c87080d232f8473b5392175c61e6dc
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-09-22 19:29:59 -0400

    [add] experiences to navbar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 const links = [
   { href: "/", label: "About" },
+  { href: "/experience", label: "Experience" },
   { href: "/projects", label: "Projects" },
 ];
 
