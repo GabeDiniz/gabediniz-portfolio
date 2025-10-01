720aa1f109f392e0ea9893d5f23cd76e901064dc
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-09-30 21:20:05 -0400

    [update] experience page props

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -26,13 +26,6 @@ const Experiences = [
   }
 ];
 
-const tags = [
-  "All",
-  "Web design",
-  "Applications",
-  "Web development"
-]
-
 export default function ExperiencesPage() {
     // write a function to retrieve set of tags
 
@@ -58,8 +51,9 @@ export default function ExperiencesPage() {
 
       {/* Grid */}
       <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
-        {Experiences.map((p) => (
-          <ExperienceCard key={p.title} title={p.title} tag={p.title} link={p.title} />
+        {Experiences.map((exp) => (
+          // remember to update key
+          <ExperienceCard key={exp.title} title={exp.title} location={exp.location} startDate={exp.startDate endDate={exp.endDate} technologies={exp.technologies highlights={exp.highlights />
         ))}
       </div>
     </section>
