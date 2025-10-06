3e3776dc586143e4e9dbf5d2616a05fda12c9265
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-06 15:14:32 -0400

    [add] new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,8 @@
 export const projects =
     [
-        { title: "Proj1", tag: "Web design", link: "/not-linked"},
-        { title: "Proj2", tag: "Applications", link: "/not-linked" },
-        { title: "Proj3", tag: "Web development", link: "/not-linked" },
+        { title: "Multi-tool Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/discord-bot-v2"},
+        { title: "GitHub Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/github-bot" },
+        { title: "SpurHacks Hackathon Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard" },
         { title: "Proj4", tag: "Automation", link: "/not-linked" },
         { title: "Proj5", tag: "Internal app", link: "/not-linked" },
         { title: "Proj6", tag: "AI/LLM", link: "/not-linked" },
