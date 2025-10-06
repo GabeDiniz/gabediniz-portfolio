668ea983b242d5757288d5c3789fb70b91b90f45
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-06 15:15:20 -0400

    [update] href target _blank

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ type Props = { title: string; tag: string, link: string };
 export default function ProjectCard({ title, tag, link }: Props) {
   return (
     <article className="group overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-900/50 hover:cursor-pointer">
-      <a href={link}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
         <div className="aspect-[16/10] w-full bg-neutral-800/60" />
         <div className="p-4">
           <h3 className="font-medium group-hover:text-yellow-400 transition-colors">
