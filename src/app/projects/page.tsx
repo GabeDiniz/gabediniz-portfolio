97ab8224a64b995b11c12ba691fab899d11dc75e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-05 20:10:48 -0400

    [update] projects var into projects data file

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,5 @@
 import ProjectCard from "@/components/ProjectCard";
-
-const projects = [
-  { title: "Proj1", tag: "Web design", link: "/not-linked"},
-  { title: "Proj2", tag: "Applications", link: "/not-linked" },
-  { title: "Proj3", tag: "Web development", link: "/not-linked" },
-  { title: "Proj4", tag: "Automation", link: "/not-linked" },
-  { title: "Proj5", tag: "Internal app", link: "/not-linked" },
-  { title: "Proj6", tag: "AI/LLM", link: "/not-linked" },
-];
+import { projects } from "@/data/projects";
 
 const tags = [
   "All",
---
+++ src/data/projects.ts
@@ -0,0 +1,9 @@
+export const projects =
+    [
+        { title: "Proj1", tag: "Web design", link: "/not-linked"},
+        { title: "Proj2", tag: "Applications", link: "/not-linked" },
+        { title: "Proj3", tag: "Web development", link: "/not-linked" },
+        { title: "Proj4", tag: "Automation", link: "/not-linked" },
+        { title: "Proj5", tag: "Internal app", link: "/not-linked" },
+        { title: "Proj6", tag: "AI/LLM", link: "/not-linked" },
+    ]
