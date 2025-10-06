621f319b0f3c9f043eb32151ebac4c5c40b5ad1f
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-05 20:09:19 -0400

    [update] move experience var to a data file

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,49 +1,5 @@
 import ExperienceCard from "@/components/ExperienceCard";
-
-const Experiences = [
-  {
-    company: "OpenText",
-    title: "Build Engineer",
-    location: "Waterloo",
-    startDate: "Jan 2023",
-    endDate: "Present",
-    technologies: [
-      "Python",
-      "Flask",
-      "GitLab",
-      "Git",
-      "REST API",
-      "YAML",
-      "CI/CD",
-      "System Hooks"
-    ],
-    highlights: [
-      "Designed and deployed a Flask-based internal API service to consume GitLab System Webhooks, asynchronously process team access events, and enforce access policies on personal repositories using a threaded queue-based architecture.",
-      "Led the migration of over 1,000+ projects from Perforce to GitLab, leveraging CI pipelines, YAML, and Python to ensure the complete preservation of all branches and history.",
-      "Collaborated with multiple development teams to streamline processes and resolve build errors. Developed Python scripts utilizing GitLab/TeamCity/Jira REST API, reducing branch creation, build configuration, and release candidate setup time from 5+ minutes to < 15 seconds, eliminating potential manual errors.",
-      "Deployed all automation scripts to Waterloo, Bangalore, and Hyderabad teams, significantly enhancing productivity and consistency across global teams."
-    ]
-  },
-  {
-    company: "Konfer x HawkHacks",
-    title: "Software Engineer (Startup)",
-    location: "Hybrid",
-    startDate: "Oct 2023",
-    endDate: "Present",
-    technologies: [
-      "Vite",
-      "TypeScript",
-      "Tailwind",
-      "Firebase",
-      "Docker"
-    ],
-    highlights: [
-      "Developed and launched a Hackathon website using Vite, Tailwind CSS, and TypeScript, which provided an interactive and user-friendly platform for participants, resulting in a seamless experience and increased engagement during the event.",
-      "Engineered a comprehensive dashboard using Firebase to offer real-time data insights and management capabilities. This dashboard was used by over 1000+ users, enhancing our ability to monitor and analyze event metrics effectively.",
-      "Collaborated with a cross-functional team to implement scalable web solutions, ensuring high performance and responsiveness. This effort significantly improved user satisfaction and contributed to a higher retention rate during and after the Hackathon."
-    ]
-  }
-];
+import { Experiences } from "@/data/experiences";
 
 export default function ExperiencesPage() {
     // write a function to retrieve set of tags
