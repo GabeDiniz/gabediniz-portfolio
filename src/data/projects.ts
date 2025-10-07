692127d335a56f0843155f3204b1ff044d6ab080
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-07 15:47:00 -0400

    [add] more projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,6 @@ export const projects =
         { title: "Multi-tool Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/discord-bot-v2", image: "/avatar.png"},
         { title: "GitHub Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/github-bot", image: "/avatar.png" },
         { title: "SpurHacks Hackathon Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard", image: "/avatar.png" },
-        { title: "Proj4", tag: "Automation", link: "/not-linked", image: "/avatar.png" },
-        { title: "Proj5", tag: "Internal app", link: "/not-linked", image: "/avatar.png" },
-        { title: "Proj6", tag: "AI/LLM", link: "/not-linked", image: "/avatar.png" },
+        { title: "SpurHacks Website", tag: "Website", link: "https://github.com/KonferCA/SpurHacks", image: "/avatar.png" },
+        { title: "Spur Innovation", tag: "Website", link: "https://github.com/KonferCA/SPUR-Innovation", image: "/avatar.png" },
     ]
