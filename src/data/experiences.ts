e8899f5d1303832b9656b1332d3df75be4196e32
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-10 16:15:02 -0400

    [add] logo link to each experience

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -7,22 +7,23 @@ export const experiences =
             startDate: "Jan 2023",
             endDate: "Present",
             technologies: [
-            "Python",
-            "Flask",
-            "GitLab",
-            "GitLab",
-            "Git",
-            "REST API",
-            "YAML",
-            "CI/CD",
-            "System Hooks"
+                "Python",
+                "Flask",
+                "GitLab",
+                "GitLab",
+                "Git",
+                "REST API",
+                "YAML",
+                "CI/CD",
+                "System Hooks"
             ],
             highlights: [
-            "Designed and deployed a Flask-based internal API service to consume GitLab System Webhooks, asynchronously process team access events, and enforce access policies on personal repositories using a threaded queue-based architecture.",
-            "Led the migration of over 1,000+ projects from Perforce to GitLab, leveraging CI pipelines, YAML, and Python to ensure the complete preservation of all branches and history.",
-            "Collaborated with multiple development teams to streamline processes and resolve build errors. Developed Python scripts utilizing GitLab/TeamCity/Jira REST API, reducing branch creation, build configuration, and release candidate setup time from 5+ minutes to < 15 seconds, eliminating potential manual errors.",
-            "Deployed all automation scripts to Waterloo, Bangalore, and Hyderabad teams, significantly enhancing productivity and consistency across global teams."
-            ]
+                "Designed and deployed a Flask-based internal API service to consume GitLab System Webhooks, asynchronously process team access events, and enforce access policies on personal repositories using a threaded queue-based architecture.",
+                "Led the migration of over 1,000+ projects from Perforce to GitLab, leveraging CI pipelines, YAML, and Python to ensure the complete preservation of all branches and history.",
+                "Collaborated with multiple development teams to streamline processes and resolve build errors. Developed Python scripts utilizing GitLab/TeamCity/Jira REST API, reducing branch creation, build configuration, and release candidate setup time from 5+ minutes to < 15 seconds, eliminating potential manual errors.",
+                "Deployed all automation scripts to Waterloo, Bangalore, and Hyderabad teams, significantly enhancing productivity and consistency across global teams."
+            ],
+            logo: "/avatar.png"
         },
         {
             company: "Konfer x HawkHacks",
@@ -31,17 +32,18 @@ export const experiences =
             startDate: "Oct 2023",
             endDate: "Present",
             technologies: [
-            "Vite",
-            "TypeScript",
-            "Tailwind",
-            "Firebase",
-            "Docker"
+                "Vite",
+                "TypeScript",
+                "Tailwind",
+                "Firebase",
+                "Docker"
             ],
             highlights: [
-            "Developed and launched a Hackathon website using Vite, Tailwind CSS, and TypeScript, which provided an interactive and user-friendly platform for participants, resulting in a seamless experience and increased engagement during the event.",
-            "Engineered a comprehensive dashboard using Firebase to offer real-time data insights and management capabilities. This dashboard was used by over 1000+ users, enhancing our ability to monitor and analyze event metrics effectively.",
-            "Collaborated with a cross-functional team to implement scalable web solutions, ensuring high performance and responsiveness. This effort significantly improved user satisfaction and contributed to a higher retention rate during and after the Hackathon."
-            ]
+                "Developed and launched a Hackathon website using Vite, Tailwind CSS, and TypeScript, which provided an interactive and user-friendly platform for participants, resulting in a seamless experience and increased engagement during the event.",
+                "Engineered a comprehensive dashboard using Firebase to offer real-time data insights and management capabilities. This dashboard was used by over 1000+ users, enhancing our ability to monitor and analyze event metrics effectively.",
+                "Collaborated with a cross-functional team to implement scalable web solutions, ensuring high performance and responsiveness. This effort significantly improved user satisfaction and contributed to a higher retention rate during and after the Hackathon."
+            ],
+            logo: "/avatar.png"
         },
         {
             company: "OpenText",
@@ -49,11 +51,18 @@ export const experiences =
             location: "Waterloo",
             startDate: "Sep 2022",
             endDate: "Dec 2022",
-            technologies: ["Next.js", "SpringBoot", "Java", "TypeScript", "SQL"],
+            technologies: [
+                "Next.js",
+                "SpringBoot",
+                "Java",
+                "TypeScript",
+                "SQL"
+            ],
             highlights: [
-            "Designed effective and scalable software for a Case Management Fullstack Application.",
-            "Resolved 20+ bug fixes and feature requests reported by product owners and customers.",
-            "Developed new features consuming RESTful APIs using TypeScript, Next.js/React.js, and SQL."
-            ]
+                "Designed effective and scalable software for a Case Management Fullstack Application.",
+                "Resolved 20+ bug fixes and feature requests reported by product owners and customers.",
+                "Developed new features consuming RESTful APIs using TypeScript, Next.js/React.js, and SQL."
+            ],
+            logo: "/avatar.png"
         }
     ];
