dd12d9b5a554841b0de9a449fba9bb68f0ab2764
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-13 20:16:13 -0400

    [update] project name for spurhacks dashboard

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const projects =
     [
         { title: "Multi-tool Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/discord-bot-v2", image: "/avatar.png"},
         { title: "GitHub Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/github-bot", image: "/avatar.png" },
-        { title: "SpurHacks Hackathon Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard", image: "/avatar.png" },
+        { title: "SpurHacks Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard", image: "/avatar.png" },
         { title: "SpurHacks Website", tag: "Website", link: "https://github.com/KonferCA/SpurHacks", image: "/avatar.png" },
         { title: "Spur Innovation", tag: "Website", link: "https://github.com/KonferCA/SPUR-Innovation", image: "/avatar.png" },
     ]
