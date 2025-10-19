31bb9dad1b79a40da207a5ba97ccaaa886d82ae6
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-10-19 14:50:40 -0400

    [update] project image links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,8 @@
 export const projects =
     [
-        { title: "Multi-tool Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/discord-bot-v2", image: "/avatar.png"},
-        { title: "GitHub Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/github-bot", image: "/avatar.png" },
-        { title: "SpurHacks Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard", image: "/avatar.png" },
-        { title: "SpurHacks Website", tag: "Website", link: "https://github.com/KonferCA/SpurHacks", image: "/avatar.png" },
-        { title: "Spur Innovation", tag: "Website", link: "https://github.com/KonferCA/SPUR-Innovation", image: "/avatar.png" },
+        { title: "Multi-tool Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/discord-bot-v2", image: "/projects/discord-bot-v2.png"},
+        { title: "GitHub Discord Bot", tag: "Bot", link: "https://github.com/GabeDiniz/github-bot", image: "/projects/github-discord-bot.jpeg" },
+        { title: "SpurHacks Dashboard", tag: "Application", link: "https://github.com/KonferCA/SpurHacks-Dashboard", image: "/projects/spurhacks-dashboard.png" },
+        { title: "SpurHacks Website", tag: "Website", link: "https://github.com/KonferCA/SpurHacks", image: "/projects/spurhacks-website.png" },
+        { title: "Spur Innovation", tag: "Website", link: "https://github.com/KonferCA/SPUR-Innovation", image: "/projects/spur-innovation-website.png" },
     ]
